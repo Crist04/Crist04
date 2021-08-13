47c09b8363fe06c6c487ef5cec1a4bee04fa8d68
Anshuman Pandey <34674708+Crist04@users.noreply.github.com>
2021-08-13 18:59:49 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -19,7 +19,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Alex Bangau Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Anshuman Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
