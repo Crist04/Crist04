c5e7927a36ccf3c41165be5c6318144c957252e9
Crist04 <p.anshuman2001@gmail.com>
2022-03-10 14:09:22 +0530

    Resume update

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -3,16 +3,16 @@ import React, { Component } from 'react';
 class Resume extends Component {
   render() {
 
-    if(this.props.data){
-      var education = this.props.data.education.map(function(education){
+    if (this.props.data) {
+      var education = this.props.data.education.map(function (education) {
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+          <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
+          <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
+      var work = this.props.data.work.map(function (work) {
         return <div key={work.title}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+          <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
+          <p>{work.description}</p>
         </div>
       })
     }
@@ -20,63 +20,66 @@ class Resume extends Component {
     return (
       <section id="resume">
 
-      <div className="row education">
-         <div className="three columns header-col">
+        <div className="row education">
+          <div className="three columns header-col">
             <h1><span>Education</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
+          <div className="nine columns main-col">
             <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
+              <div className="twelve columns">
+                {education}
+              </div>
             </div>
-         </div>
-      </div>
+          </div>
+        </div>
 
 
-      <div className="row work">
+        <div className="row work">
 
-         <div className="three columns header-col">
+          <div className="three columns header-col">
             <h1><span>Work</span></h1>
-         </div>
+          </div>
 
-         <div className="nine columns main-col">
-          {work}
-        </div>
-    </div>
-
-
-
-      <div className="row skill">
-
-        <div className="three columns header-col">
-          <h1><span>Skills</span></h1>
+          <div className="nine columns main-col">
+            {work}
+          </div>
         </div>
 
-        <div className="nine columns main-col">
 
-          <section id="four" className="skills">
-            <div className="languages skills show-on-scroll">
-              <i className="devicon-heroku-plain-wordmark technology"></i>
-              <i className="devicon-git-plain-wordmark colored technology"></i>
-              <i className="devicon-bootstrap-plain-wordmark technology"></i>
-              <i className="devicon-css3-plain-wordmark colored technology"></i>
-              <i className="devicon-html5-plain-wordmark colored technology"></i>
-              <i className="devicon-django-plain-wordmark technology"></i>
-              <i className="devicon-javascript-plain colored technology"></i>
-              <i className="devicon-postgresql-plain-wordmark  technology"></i>
-              <i className="devicon-react-plain-wordmark colored technology"></i>
-              <i className="devicon-php-plain technology"></i>
-              <i className="devicon-mysql-plain-wordmark colored technology"></i>
-              <i className="devicon-github-original technology"></i>
-	  <i className="devicon-python-plain technology"></i>
-              <i className="devicon-cplusplus-plain technology"></i>
-            </div>
-          </section>
-				</div>
-			</div>
-   </section>
+
+        <div className="row skill">
+
+          <div className="three columns header-col">
+            <h1><span>Skills</span></h1>
+          </div>
+
+          <div className="nine columns main-col">
+
+            <section id="four" className="skills">
+              <div className="languages skills show-on-scroll">
+                <i className="devicon-heroku-plain-wordmark technology"></i>
+                <i className="devicon-git-plain-wordmark colored technology"></i>
+                <i className="devicon-bootstrap-plain-wordmark technology"></i>
+                <i className="devicon-css3-plain-wordmark colored technology"></i>
+                <i className="devicon-html5-plain-wordmark colored technology"></i>
+                
+                <i className="devicon-javascript-plain colored technology"></i>
+                <i className="devicon-postgresql-plain-wordmark  technology"></i>
+                <i className="devicon-react-plain-wordmark colored technology"></i>
+                <i className="devicon-flutter-plain"></i>
+                <svg viewBox="0 0 128 128">
+                  <linearGradient id="kotlin-original-a" gradientUnits="userSpaceOnUse" x1="-11.899" y1="48.694" x2="40.299" y2="-8.322"><stop offset="0" stop-color="#1c93c1"></stop><stop offset=".163" stop-color="#2391c0"></stop><stop offset=".404" stop-color="#378bbe"></stop><stop offset=".696" stop-color="#587eb9"></stop><stop offset=".995" stop-color="#7f6cb1"></stop></linearGradient><path fill="url(#kotlin-original-a)" d="M0 0h65.4L0 64.4z"></path><linearGradient id="kotlin-original-b" gradientUnits="userSpaceOnUse" x1="43.553" y1="149.174" x2="95.988" y2="94.876"><stop offset="0" stop-color="#1c93c1"></stop><stop offset=".216" stop-color="#2d8ebf"></stop><stop offset=".64" stop-color="#587eb9"></stop><stop offset=".995" stop-color="#7f6cb1"></stop></linearGradient><path fill="url(#kotlin-original-b)" d="M128 128L64.6 62.6 0 128z"></path><linearGradient id="kotlin-original-c" gradientUnits="userSpaceOnUse" x1="3.24" y1="95.249" x2="92.481" y2="2.116"><stop offset="0" stop-color="#c757a7"></stop><stop offset=".046" stop-color="#ca5a9e"></stop><stop offset=".241" stop-color="#d66779"></stop><stop offset=".428" stop-color="#e17357"></stop><stop offset=".6" stop-color="#e97c3a"></stop><stop offset=".756" stop-color="#ef8324"></stop><stop offset=".888" stop-color="#f28817"></stop><stop offset=".982" stop-color="#f48912"></stop></linearGradient><path fill="url(#kotlin-original-c)" d="M0 128L128 0H64.6L0 63.7z"></path>
+                </svg>
+                <i className="devicon-mysql-plain-wordmark colored technology"></i>
+                <i className="devicon-github-original technology"></i>
+                <i className="devicon-python-plain technology"></i>
+                <i className="devicon-cplusplus-plain technology"></i>
+              </div>
+            </section>
+          </div>
+        </div>
+      </section>
     );
   }
 }
